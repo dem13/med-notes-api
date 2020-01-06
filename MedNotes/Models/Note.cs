698d2525f6ad6fd16509288db09dc101bb8c71d1
demyansson <idcadifi@gmail.com>
2020-01-06 14:55:13 +0200

    create note model

---
+++ MedNotes/Models/Note.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MedNotes.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public string Body { get; set; }
+
+        [DataType(DataType.DateTime)] public DateTime CreatedAt { get; set; } = new DateTime();
+    }
+}
